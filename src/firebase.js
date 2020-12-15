30ad6c16fade72aeba7f3faffefd168e4e72c1ab
Yogesh <patilyogesh4455@gmail.com>
2020-12-15 22:14:16 +0530

    all project setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDbZK789nm6d01YdxMVSkDHEYX1WSQwVKQ",
+//   authDomain: "cloan-8dadf.firebaseapp.com",
+//   projectId: "cloan-8dadf",
+//   storageBucket: "cloan-8dadf.appspot.com",
+//   messagingSenderId: "810115697783",
+//   appId: "1:810115697783:web:c6e0f7ac04cc35084fca72",
+//   measurementId: "G-E65LT0EHQ5"
+// };
